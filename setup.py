2b0e73045328f16c94aacb714cf0a391ae8150e5
Trevor Standley <trevor.standley@gmail.com>
2023-03-10 14:45:28 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='freeze_dried_data',
+    version='1.0.0',
+    description='A simple format for machine learning datasets',
+    url='https://github.com/tstand/freeze_dried_data',
+    author='trevor standley',
+    author_email='trevor.standley@gmail.com',
+    license='MIT',
+    packages=['freeze_dried_data'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
