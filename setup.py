8c374b95b63e3ecad369fb99d2fffcbbfbeb8368
Trevor Standley <trevor.standley@gmail.com>
2024-04-12 15:33:22 -0700

    Add README info to pypi
    Update the setup.py to simply read the README.md file when uploading to pypi.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='freeze_dried_data',
     version='1.2.1',
     description='A simple format for machine learning datasets',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/tstandley/freeze_dried_data',
     author='Trevor Standley',
     author_email='trevor.standley@gmail.com',
