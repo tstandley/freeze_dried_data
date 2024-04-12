d47d2bed0479eefc365d8dcf52a6052df3619f69
Trevor Standley <trevor.standley@gmail.com>
2024-04-12 13:48:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='freeze_dried_data',
-    version='1.1.0',
+    version='1.2.0',
     description='A simple format for machine learning datasets',
     url='https://github.com/tstandley/freeze_dried_data',
     author='Trevor Standley',
