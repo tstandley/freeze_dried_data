e22b000809553fce2e1257a16fa58560d6c0f48e
Trevor Standley <trevor.standley@gmail.com>
2024-05-06 11:04:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='freeze_dried_data',
-    version='2.1.1',
+    version='2.1.2',
     description='A simple format for machine learning datasets',
     long_description=long_description,
     long_description_content_type='text/markdown',
